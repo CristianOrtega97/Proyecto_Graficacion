e4257b82c3761c2262c091dc07a0507b51a44fc8
CristianOrtega97 <emanuel.ortega@hotmail.com>
2020-11-18 01:30:44 -0600

    Sector still not working - Further investigation will be performed tomorrow

--- Middle_Point.py
+++ Middle_Point.py
@@ -6,11 +6,41 @@ y_init = []
 x = int(input("Enter x :"))
 y = int(input("Enter y :"))
 rad = int(input("Enter radius: "))
+x_init.append(x)
+y_init.append(y)
 pk = 1 - rad
-x_init.append(0)
+Xk1=1
+Yk1=rad
+TwoXk1 = 2 * Xk1
+TwoYk1 = 2 * Yk1
+
+x_init.append(x+1)
+y_init.append(y)
+pk = pk + 0 + 3
+Xk1 += 1
+TwoXk1 += 2
+x += 2
+x_init.append(x)
 y_init.append(y)
 
-if(pk>0):
-    pass
-else:
-    pass
+while TwoXk1 <= TwoYk1:
+    print("PK= ",pk)
+    print("X",TwoXk1)
+    print("Y",TwoYk1)
+    if(pk<0):
+        pk = pk + TwoXk1 + 3
+        TwoXk1 += 2
+        x += 1
+        x_init.append(x)
+        y_init.append(y)    
+    else:
+        pk = pk + TwoXk1 - TwoYk1 + 5
+        TwoXk1 += 2
+        TwoYk1 -= 2
+        x += 1
+        y -= 1
+        x_init.append(x)
+        y_init.append(y) 
+
+print(x_init)
+print(y_init)
